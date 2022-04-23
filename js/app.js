6d2836287ee46beba69aaa27b0c4534de408ba56
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-04-23 02:37:29 +0000

    messing around with color themes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
-console.log('hello world!')
 
-const date = new Date();
-const year = date.getFullYear();
+const body = document.querySelector('body');
+const themeButtons = document.querySelectorAll('.theme');
 
-document.querySelector('#date').innerHTML = year;
+
+themeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.dataset.theme === 'blue') {
+            body.classList.remove('light');
+            body.classList.remove('dark');
+            body.classList.add('blue');
+        } else if(button.dataset.theme === 'dark') {
+            body.classList.remove('light');
+            body.classList.remove('blue');
+            body.classList.add('dark');
+        } else if(button.dataset.theme === 'light') {
+            body.classList.remove('dark');
+            body.classList.remove('blue');
+            body.classList.add('light');
+        }
+    })
+})
+
+
+//function get to year and display in footer
+const setYear = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    document.querySelector('#date').innerHTML = year;
+}
+
+setYear();
+
+
+
+
+

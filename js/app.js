36a158e9bb5342fb77336f901b0072cbb0fae347
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-04-23 21:44:15 +0000

    youtube api

--- js/app.js
+++ js/app.js
@@ -21,7 +21,6 @@ themeButtons.forEach(button => {
     })
 })
 
-
 //function get to year and display in footer
 const setYear = () => {
     const date = new Date();
@@ -29,6 +28,23 @@ const setYear = () => {
     document.querySelector('#date').innerHTML = year;
 }
 
+async function getVideos(){
+    const videoContainer = document.querySelector('#video-container');
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCnBq6q_POCNVwvCJ1KCNYUA&maxResults=10&order=date&key=AIzaSyDbhMyxIM5M3AUyg1TxEV7-3fnpuTR8g4c`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const videos = data.items;
+    console.log(videos);
+
+    for(let video of videos) {
+        console.log(video.snippet.title)
+        videoContainer.innerHTML += `
+            <img src="${video.snippet.thumbnails.default.url}" />
+        `
+    }
+}   
+
+getVideos();
 setYear();
 
 
